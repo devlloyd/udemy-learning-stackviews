ba51c84ba8596dbd9274a9bae6df378b750e0a9f
Devon Lloyd <devonlloyd@fb.com>
2017-04-12 17:01:04 -0500

    Initial Commit

---
+++ learning-stackview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  learning-stackview
+//
+//  Created by Devon Lloyd on 4/12/17.
+//  Copyright © 2017 Devon Lloyd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
